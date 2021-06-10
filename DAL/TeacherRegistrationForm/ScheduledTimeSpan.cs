40499c823f6bd29689b816f6dd60acf685961728
Robin Grabs <robinmgrabs@gmail.com>
2021-06-10 13:06:58 +0200

    form controller och början till view

--- DAL/TeacherRegistrationForm/ScheduledTimeSpan.cs
+++ DAL/TeacherRegistrationForm/ScheduledTimeSpan.cs
@@ -5,11 +5,15 @@ namespace DAL.TeacherRegistrationForm
 {
     public class ScheduledTimeSpan : Entity
     {
+        [Display(Name = "Veckodag")]
         public Weekday Weekday { get; set; }
 
+        [Display(Name = "Tid")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{hh:mm}", ApplyFormatInEditMode = true)]
         public DateTime Time { get; set; } // Format this to hh:mm (only 30min gaps)
+
+        [Display(Name = "Utanför schema")]
         public bool IsOutsideSchedule { get; set; }
         public TeacherRegistrationForm TeacherRegistrationForm { get; set; }
     }
---
+++ WebApp/Controllers/RegistrationFormController.cs
@@ -0,0 +1,43 @@
+﻿using DAL.TeacherRegistrationForm;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+using WebApp.Repositories;
+
+namespace WebApp.Controllers
+{
+    public class RegistrationFormController : Controller
+    {
+        private readonly ITeacherRegistrationFormRepository _formRepo;
+
+        public RegistrationFormController(ITeacherRegistrationFormRepository formRepo)
+        {
+            _formRepo = formRepo;
+        }
+
+        public ActionResult Index()
+        {
+            var viewModel = new TeacherRegistrationFormViewModel { Form = new TeacherRegistrationForm() };
+            return View(viewModel);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(TeacherRegistrationForm form)
+        {
+            if (ModelState.IsValid)
+            {
+                await _formRepo.CreateAsync(form);
+                ViewBag.SuccessMessage = _formRepo.SuccessMessage(form);
+                return View(nameof(Index), new TeacherRegistrationFormViewModel { SubmitSuccessfull = true });
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
