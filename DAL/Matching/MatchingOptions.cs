488d86fb6de227cb145228626f1c72130ebe6cf9
Robin Grabs <robinmgrabs@gmail.com>
2021-07-28 18:16:29 +0200

    modal for matches

--- DAL/Matching/MatchingOptions.cs
+++ DAL/Matching/MatchingOptions.cs
@@ -5,5 +5,12 @@
         public bool MatchOnlyOnWeeks { get; set; } = false;
         public bool MatchOnlyOnSchedule { get; set; } = false;
         public bool MatchUncertainSchedule { get; set; } = false;
+
+        public void SetAllToFalse()
+        {
+            MatchOnlyOnWeeks = false;
+            MatchOnlyOnSchedule = false;
+            MatchUncertainSchedule = false;
+        }
     }
 }
--- WebApp/Pages/RegistrationOfInterestList.razor.cs
+++ WebApp/Pages/RegistrationOfInterestList.razor.cs
@@ -1,7 +1,9 @@
-﻿using DAL.Matching;
+﻿using Blazorise;
+using DAL.Matching;
 using DAL.Registration;
 using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApp.Pages
@@ -15,6 +17,7 @@ namespace WebApp.Pages
             get { return _selectedRegistration; }
             set
             {
+                _matchOptions.SetAllToFalse();
                 _isMatchLoaded = false;
                 _matches = null;
                 _selectedRegistration = value;
@@ -24,22 +27,39 @@ namespace WebApp.Pages
         private bool _isMatchLoaded { get; set; } = false;
         private MatchingOptions _matchOptions { get; set; }
         private IEnumerable<RegistrationOfInterest> _matches { get; set; }
+        private Modal matchesModal;
 
         protected override async Task OnInitializedAsync()
         {
             _registrations = await RegistrationsService.GetAllAsync();
             _matchOptions = new();
+
+
+            // show modal during testing
+            // vvvvvvvvvvvvvvvvvvvvvvvvv
+            _selectedRegistration = _registrations.Last();
+            matchesModal.Show();
+            _isMatchLoaded = true;
         }
 
         private async Task Match()
         {
             _isMatchLoaded = true;
             _matches = await MatchService.MatchRegistrationAsync(_matchOptions, _registrations, _selectedRegistration);
+            matchesModal.Show();
+        }
+
+        private void HideModal()
+        {
+            matchesModal.Hide();
         }
 
         private void GoToDetails(int id)
         {
             NavigationManager.NavigateTo($"{nameof(RegistrationDetails)}/{id}");
         }
+
+
+
     }
 }
