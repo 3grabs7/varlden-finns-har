e2cc6519fa4c9111cc59cd8245861de2024aa206
Robin Grabs <robinmgrabs@gmail.com>
2021-08-13 17:32:49 +0200

    asp.net identity package scaffolded

---
+++ WebApp/Pages/RegistrationDetails.razor.cs
@@ -0,0 +1,30 @@
+﻿using DAL.Registration;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Services;
+
+namespace WebApp.Pages
+{
+    public partial class RegistrationDetails : ComponentBase
+    {
+        private readonly IRegistrationOfInterestService _registrationService;
+        public RegistrationDetails(IRegistrationOfInterestService registrationService)
+        {
+            _registrationService = registrationService;
+        }
+
+        [Parameter] public string Id { get; set; }
+        private RegistrationOfInterest _registration { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            if (Int32.TryParse(Id, out int id))
+            {
+                _registration = await _registrationService.GetAsync(id);
+            }
+        }
+    }
+}
---
+++ WebApp/Pages/SideBySideComparison.razor.cs
@@ -0,0 +1,33 @@
+﻿using DAL.Registration;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Services;
+
+namespace WebApp.Pages
+{
+    public partial class SideBySideComparison : ComponentBase
+    {
+        private readonly IRegistrationOfInterestService _registrationService;
+        public SideBySideComparison(IRegistrationOfInterestService registrationService)
+        {
+            _registrationService = registrationService;
+        }
+
+        public string ComparisonParameter { get; set; }
+        private RegistrationOfInterest _registration;
+        private RegistrationOfInterest _registrationComparison;
+
+        protected async override Task OnInitializedAsync()
+        {
+            var registrationIds = ComparisonParameter.Split('-').Select(x => Convert.ToInt32(x)).ToList();
+
+            _registration = await _registrationService.GetAsync(registrationIds[0]);
+            _registrationComparison = await _registrationService.GetAsync(registrationIds[1]);
+
+        }
+
+    }
+}
